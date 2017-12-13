7e6f21421ddb9e1b291d94aadb30a81167e5d08c
Eko Martantoh <eko6379@yahoo.co.id>
2017-12-13 16:23:41 +0700

    Final

--- app/src/main/java/com/ekomartantoh/recyclerview/MainActivity.java
+++ app/src/main/java/com/ekomartantoh/recyclerview/MainActivity.java
@@ -1,13 +1,104 @@
 package com.ekomartantoh.recyclerview;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ekomartantoh.recyclerview.data.oStoreContract;
+import com.ekomartantoh.recyclerview.data.oStoreDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =MainActivity.class.getSimpleName() ;
+    protected static EditText namaBarang, harga;
+    //langkah 1 membuat field database
+    private SQLiteDatabase mDb;
+    private oStoreListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        namaBarang = (EditText) findViewById(R.id.edt_Nm_Brg);
+        harga = (EditText) findViewById(R.id.edt_Hrg);
+        //rvItem = (RecyclerView) findViewById(R.id.rvItm);
+
+        //langkah2
+        oStoreDbHelper dbHelper = new oStoreDbHelper(this);
+        //langkah3
+        mDb=dbHelper.getWritableDatabase();
+        //langkah6
+        Cursor cursor = getAllBarang();
+        //langkah 11
+        RecyclerView oStoreReciclerView;
+        oStoreReciclerView = (RecyclerView) findViewById(R.id.rvItm);
+        oStoreReciclerView.setLayoutManager( new LinearLayoutManager(this));
+        mAdapter = new oStoreListAdapter(this, cursor);
+        oStoreReciclerView.setAdapter(mAdapter);
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                long id = (long) viewHolder.itemView.getTag();
+                removeBarang(id);
+                mAdapter.swapCursor(getAllBarang());
+            }
+        }).attachToRecyclerView(oStoreReciclerView);
+
     }
+
+    //langkah4
+    private Cursor getAllBarang(){
+        //langkah5
+        return mDb.query(
+                oStoreContract.oStoreEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                oStoreContract.oStoreEntry.COLUMNS_NAMA_BARANG
+        );
+    }
+
+    public void tambah_barang(View view) {
+        if (namaBarang.getText().length()==0||
+            harga.getText().length()==0)
+            return;
+        double hargaBarang = 1;
+        try {
+            hargaBarang = Integer.parseInt(harga.getText().toString());
+        }catch (Exception ex){
+            Log.e(LOG_TAG, "Filed to parse text to number"+ex.getMessage());
+        }
+        addBarang(namaBarang.getText().toString(), hargaBarang);
+        mAdapter.swapCursor(getAllBarang());
+
+    }
+
+    public long addBarang(String nama, double harga){
+        ContentValues cv= new ContentValues();
+        cv.put(oStoreContract.oStoreEntry.COLUMNS_NAMA_BARANG, nama);
+        cv.put(oStoreContract.oStoreEntry.COLUMNS_HARGA_BARANG, harga);
+        return mDb.insert(oStoreContract.oStoreEntry.TABLE_NAME, null, cv);
+    }
+    public boolean removeBarang(long id){
+        return mDb.delete(oStoreContract.oStoreEntry.TABLE_NAME,
+                oStoreContract.oStoreEntry._ID+"="+id,null )>0;
+    }
+
 }
--- app/src/main/java/com/ekomartantoh/recyclerview/oStoreListAdapter.java
+++ app/src/main/java/com/ekomartantoh/recyclerview/oStoreListAdapter.java
@@ -3,11 +3,12 @@ package com.ekomartantoh.recyclerview;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.ekomartantoh.recyclerview.data.oStoreContract;
 
 /**
  * Created by hp on 05/12/2017.
@@ -15,32 +16,49 @@ import android.widget.TextView;
 
 public class oStoreListAdapter extends RecyclerView.Adapter<oStoreListAdapter.oStoreViewHolder> {
     private Context mContext;
-    public oStoreListAdapter(Context context){
+    //langkah 7
+    private Cursor mCursor;
+    //langkah 8
+    public oStoreListAdapter(Context context, Cursor cursor){
         this.mContext = context;
+        //langkah 9
+        this.mCursor=cursor;
     }
     @Override
     public oStoreViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
         LayoutInflater inflater=LayoutInflater.from(mContext);
         View view = inflater.inflate(R.layout.listitem, parent, false);
-        return new oSoteViewHolder(view);
+        return new oStoreViewHolder(view);
     }
     @Override
     public void onBindViewHolder(oStoreViewHolder holder, int position){
-
+        if (!mCursor.moveToPosition(position))
+            return;
+        String nama =mCursor.getString(mCursor.getColumnIndex(oStoreContract.oStoreEntry.COLUMNS_NAMA_BARANG));
+        double harga = mCursor.getDouble(mCursor.getColumnIndex(oStoreContract.oStoreEntry.COLUMNS_HARGA_BARANG));
+        long id = mCursor.getLong(mCursor.getColumnIndex(oStoreContract.oStoreEntry._ID));
+        holder.namaBarang.setText(nama);
+        holder.hargaBarang.setText(String.valueOf(harga));
+        holder.itemView.setTag(id);
     }
+    //langkah 10
     @Override
-    public getItemCount(){};
+    public int getItemCount(){return mCursor.getCount();}
 
     public void swapCursor(Cursor newCursor){
-
+        if (mCursor!=null) mCursor.close();
+        mCursor=newCursor;
+        if(newCursor!=null){
+            this.notifyDataSetChanged();
+        }
     }
     class oStoreViewHolder extends RecyclerView.ViewHolder{
         TextView namaBarang;
         TextView hargaBarang;
         public oStoreViewHolder(View itemView){
             super(itemView);
-            namaBarang = (TextView) itemView.findViewById(R.id.nmBrg);
-            hargaBarang = (TextView) itemView.findViewById(R.id.hrg);
+            namaBarang = (TextView) itemView.findViewById(R.id.Nm_Brg);
+            hargaBarang = (TextView) itemView.findViewById(R.id.Hrg);
         }
     }
 }
